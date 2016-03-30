477fa7c75596157b53d2a25894074752ea3f4d29
Brandon A. Vicinus <bvicinus@mail.usf.edu>
2016-03-29 20:48:42 -0400

    1st commit

---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Entity Framework</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+
+
+
+        <br />
+        <br />
+        <br />
+        <asp:DropDownList ID="ddlCompanies" runat="server" AppendDataBoundItems="True" AutoPostBack="True" 
+            DataSourceID="EntityDataSource1" DataTextField="CompanyName" DataValueField="SupplierID" 
+            Height="16px" OnSelectedIndexChanged="ddlCompanies_SelectedIndexChanged" Width="180px">
+            <asp:ListItem Value="0">Select Supplier</asp:ListItem>
+        </asp:DropDownList>
+        <asp:EntityDataSource ID="EntityDataSource1" runat="server" ConnectionString="name=NorthwindEntities" 
+            DefaultContainerName="NorthwindEntities" EnableFlattening="False" EntitySetName="Suppliers" Select="it.[SupplierID], it.[CompanyName]">
+        </asp:EntityDataSource>
+&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblOutput" runat="server"></asp:Label>
+    
+
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿//brandon vicinus - march 29,2016
+
+using System;
+using System.Linq;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblOutput.Text = ""; //erase the text already on the page 
+    }
+
+    protected void ddlCompanies_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCompanies.SelectedIndex == 0)
+        {
+            return; //if the user selects the dummy variable in the ddl, 
+        }
+
+        var supplier = ddlCompanies.SelectedItem.Text;
+        //need an integer variable of the supplier name to compare in the LINQ comparison
+        int supplierInt = Convert.ToInt32(ddlCompanies.SelectedValue);
+        using (NorthwindEntities myEntities = new NorthwindEntities())
+        {
+            Decimal base_cost = 0.0M;
+            Decimal discount_amt = 0.0M;
+            Decimal total_value = 0.0M;
+            int counter = 0;
+
+            //LINQ Query 
+            var products =
+                from product in myEntities.Products
+                from orderDetail in myEntities.Order_Details
+                where product.SupplierID == supplierInt &&
+                   product.ProductID == orderDetail.ProductID
+                select orderDetail;
+
+            foreach (Order_Detail orderDtl in products) //add up the total cost 
+            {
+                base_cost = Decimal.Round(orderDtl.UnitPrice * orderDtl.Quantity, 2);
+                discount_amt = Decimal.Round(base_cost * (Decimal)orderDtl.Discount, 2);
+                total_value += base_cost - discount_amt; 
+                counter++; //and keep track of the number of items 
+            }
+
+            lblOutput.Text = string.Format //display the output 
+                ("Supplier {0} has {1} order items worth {2}"
+                , supplier, counter, total_value.ToString("C2"));
+
+        }//end using 
+
+    }//end ddl function 
+
+}//end class
